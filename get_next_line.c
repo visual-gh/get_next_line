40ccb6ef4b7003a6b1e7dec04e20cc2b946fe106
visual-gh <github.molecular850@passmail.net>
2025-11-27 20:06:53 +0000

    wip

--- get_next_line.c
+++ get_next_line.c
@@ -6,22 +6,57 @@
 /*   By: Visual <github.com/visual-gh>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/27 18:17:21 by Visual            #+#    #+#             */
-/*   Updated: 2025/11/27 18:34:11 by Visual           ###   ########.fr       */
+/*   Updated: 2025/11/27 20:05:42 by Visual           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+static char	*read_to_stash(int fd, char *stash)
+{
+	char	*buf;
+	size_t	bytes;
+
+	buf = malloc(BUFFER_SIZE + 1);
+	if (!buf)
+		return (NULL);
+	bytes = 1;
+	while (!ft_strchr(stash, '\n') && bytes > 0)
+	{
+		bytes = read(fd, buf, BUFFER_SIZE);
+		if (bytes < 0)
+		{
+			free(buf);
+			return (NULL);
+		}
+		buf[bytes] = '\0';
+		if (!stash)
+			stash = ft_strdup(buf);
+		else
+			stash = ft_strjoin(stash, buf);
+		if (!stash)
+			break ;
+	}
+	free(buf);
+	return (stash);
+}
+
+
+static char	*extract_line(char *stash)
+
+static char	*clean_stash(char *stash)
+
 char	*get_next_line(int fd)
 {
-	static char	*line;
-	char		*buf;
+	static char	*stash;
+	char		*line;
 
-	if (fd < 0 || BUFFER_SIZE < 1 || read(fd, 0, 0) < 0)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	line = read_line(fd, line);
-	if (!line || !*line)
-		return (free(line), line = NULL, NULL);
-	buf = get_line(&line);
-	return (buf);
+	stash = read_to_stash(fd, stash);
+	if (!stash)
+		return (NULL);
+	line = extract_line(stash);
+	stash = clean_stash(stash);
+	return (line);
 }
